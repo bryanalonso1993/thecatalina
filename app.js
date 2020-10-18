28e7e0bb50e5b2ff6d814993917190f1c118626b
bryanalonso1993 <bryalmeyda.7@gmail.com>
2020-10-18 14:12:00 -0500

    app de la aplicacion

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express =require('express');
 const hbs = require('hbs');
+const wiki = require('./routes/wiki');
 const port = process.env.PORT || 3000;
 const app = express();
+
+// export routes
+app.use('/wiki',wiki);
 
 app.listen(port, ()=> {
     console.log(`Server running on port ${port}`);
@@ -11,11 +15,15 @@ app.set('view engine','hbs');
 
 // midleware public
 app.use(express.static('public'));
-hbs.registerPartials(__dirname+'/views');   
+hbs.registerPartials(__dirname+'/views/templates');   
 
 //methods
  app.get('/', (req,res)=> {
     console.log(__dirname);
     //res.send('Hello Word');
     res.render(__dirname+'/views/index.hbs');
+});
+
+app.get('/about', (req,res)=>{
+    res.render(__dirname+'/views/about.hbs');
 });
